1cc331776655b83a5f729502ce32f06f2b7afdac
jsdodiya <jinendradodiya9@gmail.com>
2025-01-03 10:54:51 +0530

    all

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import { use } from 'react'
-
+import Products from './products'
 function App() {
   var [a,b] = useState(69)
   return (
-    <div className='w-full h-screen bg-zinc-900 text-white p-4'>
-      <h1>{a}</h1>
-      <button onClick={()=>b(a+2)}className='px-3 py-1 rounded-xl bg-green-500'>Click</button>
+    <div className='w-full h-screen bg-zinc-900 text-white p-4 '>
+     <Products num="value"/>
+
     </div>
   )
 }
--- src/Products.jsx
+++ src/Products.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-function products() {
+function products({num}) {
   return (
-    <div className='text-white'>
-        <div className='w-full h-96 bg-slate-500'></div>
-    </div>
+   
+      <div className='text-white w-full h-60 bg-zinc-800'>
+        <h1>{num}</h1>
+      </div>
+      
     
   )
 }
