67aec26f0be27f2fa59134decb7d7013a8c0db37
jsdodiya <jinendradodiya9@gmail.com>
2025-01-04 08:12:29 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react'
-import { use } from 'react'
-import Products from './products'
-function App() {
-  var [a,b] = useState(69)
-  return (
-    <div className='w-full h-screen bg-zinc-900 text-white p-4 '>
-     <Products num="value"/>
+import React from 'react'
 
+import Main from './components/main'
+
+function App() {
+  return (
+    <div>
+      <Main/>
     </div>
   )
 }
---
+++ src/components/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './nav'
+
+const main = () => {
+  return (
+    <div className='bg-indigo-600'>
+      <Nav/>
+    </div>
+  )
+}
+
+export default main
