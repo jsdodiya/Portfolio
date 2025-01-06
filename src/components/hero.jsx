be4b6b320c4ad3b100afc4332ccdca35259c8ec6
jsdodiya <jinendradodiya9@gmail.com>
2025-01-06 16:26:24 +0530

    hero section

---
+++ src/components/hero.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {BsInstagram,BsLinkedin } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
+import JinendraImage from '../assets/image.png'
+
+
+const hero = () => {
+  return (
+    <section className='flex justify-around items-center p-10 space-x-10 lg:flex-row ssm:flex-col ssm:space-y-10 text-white'>
+        <div className='lg:w-1/3 ssm:w-fit'>
+            <p className='text-4xl mb-5 text-slate-300'> I'm</p>
+            <h1 className='text-6xl'>Jinendra Pal Dodiya</h1>
+            <hr/>
+            <p className='mt-10  text-slate-300 font-sans '>
+            Creative frontend developer dedicated to crafting seamless,
+            user-centric web interfaces that blend functionality with stunning visuals,
+            using the latest technologies and design principles.   
+            </p>
+        </div>
+        <div className='w-1/3 items-center ssm:w-fit'>
+          <img src={JinendraImage} alt='' width={300} height={300} className='rounded-full  border-8 border-white '/>  
+        </div>
+       
+        <div className='w-1/3 ssm:w-fit'>
+            <p className='text-4xl mb-4'>About Me</p>
+            <p className='text-slate-300'>
+                Let's build quality in programming and desgin with our services
+            </p>
+            <button className='bg-white text-indigo-600 px-10 py-2 my-3 rounded-full hover:bg-indigo-800 hover:text-white'>
+                Show more...
+            </button>
+
+            <div className='flex mt-5 space-x-4 cursor-pointer'>
+                <BsInstagram size={40} className='border-4 hover:border-indigo-800 rounded-full'/>
+                <BsLinkedin size={40} className='border-4 hover:border-indigo-800 rounded-full'/>
+                <FaGithub size={40} className='border-4 hover:border-indigo-800 rounded-full'/>
+                
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default hero
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from './nav'
+import Hero from './hero'
 
 const main = () => {
   return (
     <div className='bg-indigo-600'>
       <Nav/>
+      <Hero/>
     </div>
   )
 }
